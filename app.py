2de18b5513866f42a347a14774f0e57d9b1fe276
Craig Gent <genty83@outlook.com>
2024-06-19 13:46:21 +0000

    Added new function

---
+++ app.py
@@ -0,0 +1,23 @@
+"""
+ Main app file: Contains all the flask funcionality 
+for running the application 
+"""
+
+# IMPORTS
+import os
+from flask import render_template
+from src import app
+
+# Home page
+app.route("/")
+def home():
+    return render_template("home.html")
+
+
+
+if __name__ == "__main__":
+    app.run(
+        host = os.environ.get("IP"),
+        port = int(os.environ.get("PORT")),
+        debug = os.environ.get("DEBUG")
+    )
