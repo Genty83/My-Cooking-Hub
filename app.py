cbc1fc879d5c65bb31a3f24bf6657ef1395c237c
Craig Gent <genty83@outlook.com>
2024-06-20 18:31:44 +0000

    Added new function

--- app.py
+++ app.py
@@ -15,6 +15,12 @@ def home():
     return render_template("home.html")
 
 
+# View My Recipes page
+@app.route("/view_my_recipes")
+def view_my_recipes():
+    return render_template("view_my_recipes.html")
+
+
 
 if __name__ == "__main__":
     app.run(
